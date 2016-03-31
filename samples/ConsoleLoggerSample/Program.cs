b99b8b1876ab6ba80ac8afa6cdaa41dbbdde925f
Steve Smith <steve@kentsmiths.com>
2016-03-31 11:04:42 -0400

    Updated project with console sample and countlogger.

--- samples/ConsoleLoggerSample/Program.cs
+++ samples/ConsoleLoggerSample/Program.cs
@@ -1,14 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using Ardalis.Logging;
 
 namespace ConsoleLoggerSample
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            int maxNumber = 100000;
+            Console.WriteLine($"Testing primes up to {maxNumber}.");
+            var calculator = new PrimeCalculator();
+            Console.WriteLine("Naive:");
+            for (int i = 2; i < maxNumber; i++)
+            {
+                calculator.IsPrimeNaive(i);
+            }
+            Console.WriteLine("Optimized:");
+            for (int i = 2; i < maxNumber; i++)
+            {
+                calculator.IsPrimeOptimized(i);
+            }
+            CountLogger.DumpResults(Console.WriteLine);
+        }
+    }
+
+    public class PrimeCalculator
+    {
+        public bool IsPrimeNaive(int number)
+        {
+            var timer = Stopwatch.StartNew();
+            try
+            {
+                if (number < 2) return false;
+                for (int i = 2; i < number; i++)
+                {
+                    if (IsDivisibleBy(number, i)) return false;
+                }
+                return true;
+            }
+            finally
+            {
+                timer.Stop();
+                CountLogger.AddDuration(nameof(IsPrimeNaive), timer.ElapsedMilliseconds);
+            }
+        }
+        public bool IsPrimeOptimized(int number)
+        {
+            var timer = Stopwatch.StartNew();
+            try
+            {
+                if (number < 2) return false;
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (IsDivisibleBy(number, i)) return false;
+                }
+                return true;
+            }
+            finally
+            {
+                timer.Stop();
+                CountLogger.AddDuration(nameof(IsPrimeOptimized), timer.ElapsedMilliseconds);
+            }
+        }
+
+        public static bool IsDivisibleBy(int candidate, int divisor)
+        {
+            using (new CountLogger.DisposableStopwatch(nameof(IsDivisibleBy), CountLogger.AddDuration))
+            {
+                return candidate % divisor == 0;
+            }
         }
     }
 }
---
+++ src/Ardalis.Logging/CountLogger.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Ardalis.Logging
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class CountLogger
+    {
+        private static readonly Dictionary<string, MethodStats> _countLog = new Dictionary<string, MethodStats>();
+
+        public static void AddDuration(string methodName, long duration)
+        {
+            if (!_countLog.ContainsKey(methodName))
+            {
+                var stats = new MethodStats(methodName);
+                stats.AddCall(duration);
+                _countLog.Add(methodName, stats);
+            }
+            else
+            {
+                var stats = _countLog[methodName];
+                stats.AddCall(duration);
+            }
+        }
+
+        public static void AddDuration(string methodName, TimeSpan duration)
+        {
+            AddDuration(methodName, (long)duration.TotalMilliseconds);
+        }
+
+        private class MethodStats
+        {
+            public MethodStats(string methodName)
+            {
+                MethodName = methodName;
+            }
+            public string MethodName { get; }
+            public long CallCount { get; set; }
+            public long DurationTotal { get; set; }
+
+            public void AddCall(long duration)
+            {
+                CallCount++;
+                DurationTotal += duration;
+            }
+
+            public override string ToString()
+            {
+                return $"{MethodName} duration {DurationTotal} ms, Count {CallCount}";
+            }
+        }
+
+        public static void DumpResults(Action<string> outputAction)
+        {
+
+            foreach (var keyvaluepair in _countLog.OrderBy(c => c.Key))
+            {
+                var stats = keyvaluepair.Value;
+                outputAction(stats.ToString());
+            }
+        }
+
+        public class DisposableStopwatch : IDisposable
+        {
+            private readonly Stopwatch _sw;
+            private readonly string _methodName;
+            private readonly Action<string, TimeSpan> f;
+
+            public DisposableStopwatch(string methodName, Action<string, TimeSpan> f)
+            {
+                _methodName = methodName;
+                this.f = f;
+                _sw = Stopwatch.StartNew();
+            }
+
+            public void Dispose()
+            {
+                _sw.Stop();
+                f(_methodName, _sw.Elapsed);
+            }
+        }
+    }
+}
